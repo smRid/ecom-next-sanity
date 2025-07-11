cf11df4b30ac9cdf4b4de10ddb7bfa11e26aada5
smRid <omorsk911@gmail.com>
2025-07-11 21:26:51 +0600

    Add sales schema type to define sale documents with relevant fields

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -4,8 +4,9 @@ import {blockContentType} from './blockContentType'
 import {categoryType} from './categoryType'
 import productTypes from './productType'
 import orderType from './orderType'
+import salesType from './salesType'
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, productTypes, orderType],
+  types: [blockContentType, categoryType, productTypes, orderType, salesType],
 }
