b4e8437db567350428f9c4e7d615c7ed9da2b7d2
smRid <omorsk911@gmail.com>
2025-07-11 16:44:29 +0600

    Add schema definitions for author, category, and product types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Shopinity - E-commerce Store",
@@ -26,9 +16,7 @@ export default function RootLayout({
   return (
     <ClerkProvider dynamic>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body>
           {children}
         </body>
       </html>
