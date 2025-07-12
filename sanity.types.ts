f656d72952e49e7ca4a85ad18a8e60e2bd6578a4
smRid <omorsk911@gmail.com>
2025-07-12 17:24:58 +0600

    Add category page and implement products fetching by category

--- sanity.types.ts
+++ sanity.types.ts
@@ -425,6 +425,71 @@ export type PRODUCT_BY_ID_QUERYResult = {
   Stock?: number;
 } | null;
 
+// Source: ./src/sanity/lib/products/getProductsByCategory.ts
+// Variable: PRODUCTS_BY_CATEGORY_QUERY
+// Query: *[      _type == "product"      && references(*[_type == "category" && slug.current == $categorySlug].__id)    ] | order(name asc)
+export type PRODUCTS_BY_CATEGORY_QUERYResult = Array<{
+  _id: string;
+  _type: "product";
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  name?: string;
+  slug?: Slug;
+  image?: {
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    media?: unknown;
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    _type: "image";
+  };
+  description?: Array<{
+    children?: Array<{
+      marks?: Array<string>;
+      text?: string;
+      _type: "span";
+      _key: string;
+    }>;
+    style?: "blockquote" | "h1" | "h2" | "h3" | "h4" | "normal";
+    listItem?: "bullet";
+    markDefs?: Array<{
+      href?: string;
+      _type: "link";
+      _key: string;
+    }>;
+    level?: number;
+    _type: "block";
+    _key: string;
+  } | {
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    media?: unknown;
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    alt?: string;
+    _type: "image";
+    _key: string;
+  }>;
+  price?: number;
+  category?: Array<{
+    _ref: string;
+    _type: "reference";
+    _weak?: boolean;
+    _key: string;
+    [internalGroqTypeReferenceTo]?: "category";
+  }>;
+  Stock?: number;
+}>;
+
 // Source: ./src/sanity/lib/products/searchProductsByName.ts
 // Variable: PRODUCT_SEARCH_QUERY
 // Query: *[    _type == "product"    && name match $searchParam  ] | order(name asc)
@@ -515,6 +580,7 @@ declare module "@sanity/client" {
     "*[ _type == \"category\"] | order(name asc)": ALL_CATEGORIES_QUERYResult;
     "\n    *[\n      _type == \"product\"\n    ] | order(name asc)\n  ": ALL_PRODUCTS_QUERYResult;
     "\n    *[\n        _type == \"product\" && slug.current == $slug ] | order(name asc) [0]\n  ": PRODUCT_BY_ID_QUERYResult;
+    "\n    *[\n      _type == \"product\"\n      && references(*[_type == \"category\" && slug.current == $categorySlug].__id)\n    ] | order(name asc)\n  ": PRODUCTS_BY_CATEGORY_QUERYResult;
     "*[\n    _type == \"product\"\n    && name match $searchParam\n  ] | order(name asc)": PRODUCT_SEARCH_QUERYResult;
     "*[\n    _type == \"sale\"\n    && isActive == true\n    && couponCode == $couponCode\n  ] | order(validFrom desc) [0]\n   ": ACTIVE_SALE_BY_COUPON_QUERYResult;
   }
